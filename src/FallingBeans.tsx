cfb272ccce2e97ce2b1c9ee01f75518a6e481132
Ryan Geyer <ryan@waymark.com>
2021-11-06 14:56:54 -0400

    Create a whole new array when the bean count changes rather than messing around with the length of the existing one

--- src/FallingBeans.tsx
+++ src/FallingBeans.tsx
@@ -3,17 +3,14 @@ import { useThree } from "@react-three/fiber";
 
 import CoffeeBean from "./CoffeeBean";
 
-const beanCountArray = new Array();
+let beanCountArray = new Array();
 
 export default function FallingBeans() {
   const { size } = useThree();
 
-  const oldBeanCount = beanCountArray.length;
-  beanCountArray.length = Math.floor((size.width * size.height) / 10000);
-
-  if (oldBeanCount < beanCountArray.length) {
-    // Ensure we fill in all of the new slots in the array so they won't be skipped
-    // by beanCountArray.map()
+  const beanCount = Math.floor((size.width * size.height) / 10000);
+  if (beanCount !== beanCountArray.length) {
+    beanCountArray = new Array(beanCount);
     beanCountArray.fill(null);
   }
 
